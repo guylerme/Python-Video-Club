2adeabe739fbcff317da79a6baaaad606ac6016d
unknown <lab_10@.(none)>
2011-02-18 19:57:37 -0300

    adicionando no controle de versao

---
+++ Python Video Club/Socio_Test.py
@@ -0,0 +1,18 @@
+import unittest
+from socio import Socio
+
+class Socio_Test(unittest.TestCase):
+    def testSocioName(self):
+        socio = Socio('Teste', 1, '27223322')
+        assert socio.nome != None, "nao tem nome"
+
+    def testSocioInscricao(self):
+        socio = Socio('Teste',1, None)
+        assert socio.inscricao != None, "nao tem inscricao!"
+
+    def testSocioTelefone(self):
+        socio = Socio('Teste',1, '27223344')
+        assert socio.telefone != None, "nao tem telefone!"
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Python Video Club/socio.py
@@ -0,0 +1,6 @@
+class Socio():
+
+    def __init__(self, v_nome, v_inscricao, v_telefone):
+        self.nome = v_nome
+        self.inscricao = v_inscricao
+        self.telefone = v_telefone
