221eab9469838412c2d868474afd82341d809a28
Guylerme Velasco <guylerme@gmail.com>
2011-02-26 18:19:27 -0300

    Testando interface grafica

--- fontes/principal.py
+++ fontes/principal.py
@@ -1,9 +1,9 @@
 import _tkinter
 import tkinter
 
-import Emprestimo
-import Copia
-import Socio
+##import Emprestimo
+##import Copia
+##import Socio
 
 class Principal():
     def __init__(self):
@@ -22,7 +22,7 @@ class Principal():
         self.listaCopia.append(c)
         c = Copia(2, 'As trancas do careca', '1:30', 2008, 'Comedia', 'Steven Spilberg', 'Brad Pitt', 'Angelina Jolie', '25/12/2009', 'ruim', 5167)
         self.listaCopia.append(c)
-        c = Copia(3, 'As trancas do careca', '1:30', 2008, 'ComÃƒÂ©dia', 'Steven Spilberg', 'Brad Pitt', 'Angelina Jolie', '10/02/2010', 'bom', 5167)
+        c = Copia(3, 'As trancas do careca', '1:30', 2008, 'ComÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â©dia', 'Steven Spilberg', 'Brad Pitt', 'Angelina Jolie', '10/02/2010', 'bom', 5167)
         self.listaCopia.append(c)
 
         c = Copia(4, 'O pistoleiro sem dedo', '2:00', 2010, 'Acao', 'Dalton Trumbo', 'Jackie Chan', 'Antonio Banderas', '05/08/2010', 'bom', 5182)
@@ -94,7 +94,8 @@ class Principal():
         self.listaSocios.append(self.s)
 
     def inicializaMenu(self):
-        frame = tkinter.Frame(root)
+        root = tkinter.Tk()
+        frame = tkinter.Frame()
         frame.pack()
 
         self.button = tkinter.Button(frame, text="QUIT", fg="red",
@@ -104,6 +105,10 @@ class Principal():
         self.hi_there = tkinter.Button(frame, text="Hello",
                            command=self.say_hi)
         self.hi_there.pack(side=tkinter.LEFT)
+        self.label = tkinter.Label(frame, text="Test")
+        self.label.pack(side=tkinter.LEFT)
+
+        root.mainloop()
 
     def say_hi(self):
-        return "hi there, everyone!"
+        print("hi there, everyone!")
