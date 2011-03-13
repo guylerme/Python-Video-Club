4fe408f72e3686f142848888d6a7c5582b574a80
Alexandre Paiva <aspaiva@gmail.com>
2011-03-13 20:51:50 -0300

    Incluindo teste assertRaises

--- fontes/Copia_Test.py
+++ fontes/Copia_Test.py
@@ -1,11 +1,13 @@
 import unittest
 import datetime
 import time
-from copia import Copia
+import copia
+##from copia import Copia
+##from copia import errCopiaMissingValues
 
 class Copia_Test(unittest.TestCase):
-    copia =  Copia(1, 'As trancas do careca', '2:30', 2009, 'drama',
-     'Jim Carey', 'Brad Pitt', 'Angelina Jolie', '18/03/2011', 'bom', 5135.2)
+    copia =  copia.Copia(1, 'As trancas do careca', '2:30', 2009, 'drama',
+     'Jim Carey', 'Brad Pitt', 'Angelina Jolie', '18/03/2011', 'OTIMA', 5135.2)
 
     def testCopiaAno(self):
         """Erro ao configurar o ano"""
@@ -16,10 +18,17 @@ class Copia_Test(unittest.TestCase):
         self.assertEqual(int(self.copia.concine), self.copia.concine)
 
     def testCopiaDataValida(self):
-        """Data deve ser válida e ser <= hoje"""
+        """Data deve ser valida e ser <= hoje"""
         hoje = datetime.datetime.today()
         data = datetime.datetime(*time.strptime(self.copia.dataAquisicao, "%d/%m/%Y")[0:5])
-        assert data <= hoje, "Data deve ser válida e ser <= hoje"
+        assert data <= hoje, "Data deve ser valida e ser <= hoje"
+
+    def testCopiaEstado(self):
+        """Verifica se o estado da fita ÃƒÂ© bom ou ruim"""
+        assert self.copia.estado.lower() == "bom" or self.copia.estado.lower() == "ruim", 'Estado da copia deve ser bom ou ruim'
+
+    def testCopiaStrings(self):
+        self.assertRaises(copia.errCopiaMissingValues, self.copia.setElenco, '','','')
 
     def suite():
         suite = unittest.TestSuite()
--- fontes/copia.py
+++ fontes/copia.py
@@ -1,3 +1,7 @@
+#Definir as classes de erro a serem disparados pela classe
+##class CopiaError(Exception): pass
+class errCopiaMissingValues(Exception): pass
+
 class Copia():
     def __init__(self, v_codigo, v_titulo, v_duracao, v_ano,
         v_genero, v_nomeDiretor, v_artista1, v_artista2, v_dataAquisicao,
@@ -14,3 +18,8 @@ class Copia():
         self.estado = v_estado
         self.concine = v_concine
         #self.newattr = v_newattr
+
+    def setElenco(self, p_art1, p_art2, p_diretor):
+        """Setar os nomes principais do elenco e do diretor"""
+        pass
+
